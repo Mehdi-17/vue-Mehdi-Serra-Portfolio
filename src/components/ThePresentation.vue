52f7decd34c1b00718df5e86b986b1252a65c1ce
Mehdi <mehdi.serra@prestataire.sihm.fr>
2022-10-29 19:50:06 +0200

    add linkedin link

--- src/components/ThePresentation.vue
+++ src/components/ThePresentation.vue
@@ -12,11 +12,14 @@ defineProps({
     <h1 class="green">{{ msg }}</h1>
     <h2>💻Développeur Full Stack</h2>
     <h3>☄️Java 🚀Spring Boot 🪐Angular</h3>
+    <h4>
+      <a href="https://www.linkedin.com/in/mehdi-serra-5a3489207/" target="_blank">⚡Voir mon profil LinkedIn</a>
+    </h4>
   </div>
 </template>
 
 <style scoped>
-h1,h2, h3 {
+h1,h2, h3, h4 {
   text-align: center;
 }
 
